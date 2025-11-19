2e120bb9fff2dee6290b4243953a94265137879d
PaulLandsmann <p.landsmann@online-home.de>
2025-11-19 17:55:27 +0100

    p1 fertig woche 5

---
+++ src/p1/P1_main.java
@@ -0,0 +1,28 @@
+package p1;
+
+public class P1_main {
+
+	public static void main(String[] args) {
+		
+		int x = 0;
+		int y = 0;
+		
+		while (x <= 10) {
+			x = x + 1;
+			System.out.println(x);
+		}
+		
+		for(int i = 1; i <= 11; i++) {
+			System.out.println(i);
+		}
+		
+		
+		do {
+			y++;
+			System.out.println(y);
+		} while(y <= 10);
+			
+
+	}
+
+}
